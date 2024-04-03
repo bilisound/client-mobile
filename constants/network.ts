34fe9e54deee84e1caefb32abccb834ecbbc92e4
tcdw <tcdw2011@gmail.com>
2024-04-03 18:25:42 +0800

    更新常量

--- constants/network.ts
+++ constants/network.ts
@@ -1,8 +1,8 @@
 export const USER_AGENT_BILIBILI =
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36";
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36";
 
 export const USER_AGENT_BILISOUND = `Bilisound/${require("../package.json").version}`;
 
 export const BILIBILI_VIDEO_URL_PREFIX = "https://www.bilibili.com/video/";
 
-export const BILIBILI_GOOD_CDN_REGEX: ReadonlyArray<RegExp> = [/^upos-[hs]z-[a-z|0-9]+\..+\.[a-z]+$/];
+export const BILIBILI_GOOD_CDN_REGEX: readonly RegExp[] = [/^upos-[hs]z-[a-z|0-9]+\..+\.[a-z]+$/];
