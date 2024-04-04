37f9eb77096d3e97f86a32cee75efd766b34298d
tcdw <tcdw2011@gmail.com>
2024-04-04 20:05:34 +0800

    增加播放列表页面

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,19 @@
-import { Ionicons, FontAwesome5 } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
-import { View } from "react-native";
-
-import React from "react";
+import { Ionicons, FontAwesome5, Entypo } from "@expo/vector-icons";
+import { Box } from "@gluestack-ui/themed";
 import { BottomTabBarProps } from "@react-navigation/bottom-tabs/src/types";
 import BottomTabBar from "@react-navigation/bottom-tabs/src/views/BottomTabBar";
-import { Box } from "@gluestack-ui/themed";
-import YuruChara from "../../components/YuruChara";
+import { Tabs } from "expo-router";
+import React from "react";
+import { View } from "react-native";
+
 import AudioPlayerMini from "../../components/AudioPlayerMini";
+import YuruChara from "../../components/YuruChara";
 import useCommonColors from "../../hooks/useCommonColors";
 
 const SearchIcon = ({ color }: { color: string }) => <FontAwesome5 name="search" size={20} color={color} />;
 const HistoryIcon = ({ color }: { color: string }) => <Ionicons name="play-circle" size={24} color={color} />;
 const SettingsIcon = ({ color }: { color: string }) => <Ionicons name="settings-sharp" size={22} color={color} />;
+const ListIcon = ({ color }: { color: string }) => <Entypo name="list" size={22} color={color} />;
 
 const TabBar = (props: BottomTabBarProps) => (
     <View>
@@ -58,6 +59,14 @@ const TabLayout = () => {
                     options={{
                         title: "正在播放",
                         tabBarIcon: HistoryIcon,
+                        headerShown: false,
+                    }}
+                />
+                <Tabs.Screen
+                    name="playlist"
+                    options={{
+                        title: "播放列表",
+                        tabBarIcon: ListIcon,
                         headerShown: false,
                     }}
                 />
---
+++ app/(tabs)/playlist.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@gluestack-ui/themed";
+
+import CommonFrameNew from "../../components/CommonFrameNew";
+
+export default function Page() {
+    return (
+        <CommonFrameNew title="播放列表" titleBarTheme="transparent">
+            <Text>播放列表页面（施工中）</Text>
+        </CommonFrameNew>
+    );
+}
