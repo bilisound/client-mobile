4bb974a46219d4e08d82592fc01706253162e19f
tcdw <tcdw2011@gmail.com>
2024-04-25 18:24:50 +0800

    减少重复保存次数

--- hooks/useTracks.tsx
+++ hooks/useTracks.tsx
@@ -1,30 +1,33 @@
-import { useEffect, useState } from "react";
-import { Track } from "react-native-track-player";
+import { useQuery } from "@tanstack/react-query";
+import { useCallback, useEffect } from "react";
 import TrackPlayer, { useActiveTrack } from "react-native-track-player";
-import { saveTrackData } from "../utils/track-data";
+
 import log from "../utils/logger";
+import { saveTrackData } from "../utils/track-data";
 
 const useTracks = () => {
-    const [tracks, setTracks] = useState<Track[]>([]);
+    const { data, refetch } = useQuery({
+        queryKey: ["tracks"],
+        queryFn: TrackPlayer.getQueue,
+    });
+
     const activeTrack = useActiveTrack();
 
-    const update = async () => {
-        setTracks(await TrackPlayer.getQueue());
+    const update = useCallback(async () => {
+        await refetch();
         try {
             await saveTrackData();
             log.debug("播放列表保存成功");
         } catch (e) {
             log.error(`播放列表保存失败。错误信息：${e}`);
         }
-    };
+    }, [refetch]);
 
     useEffect(() => {
-        (async () => {
-            await update();
-        })();
-    }, [activeTrack]);
+        update();
+    }, [activeTrack, update]);
 
-    return { tracks, update };
+    return { tracks: data || [], update };
 };
 
 export default useTracks;
