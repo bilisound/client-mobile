c5b5409c304cb7859623941a8a306025bfd399cc
tcdw <tcdw2011@gmail.com>
2024-07-27 22:37:01 +0800

    统一使用 promise-memoize

--- api/external/direct.ts
+++ api/external/direct.ts
@@ -1,10 +1,12 @@
+import promiseMemoize from "promise-memoize";
+
 import { getVideoUrlFestival } from "~/api/external/json";
 import { InitialStateFestivalResponse, InitialStateResponse, WebPlayInfo } from "~/api/external/types";
 import { BILIBILI_VIDEO_URL_PREFIX, USER_AGENT_BILIBILI } from "~/constants/network";
 import log from "~/utils/logger";
 import { extractJSON } from "~/utils/string";
-import ExpiringMap from "~/utils/timeout-map";
-import { encWbi, getWbiKeys } from "~/utils/wbi";
+
+const maxAge = 600_000;
 
 export interface GetVideoResponse {
     type: "regular";
@@ -18,63 +20,45 @@ export interface GetVideoFestivalResponse {
     playInfo: WebPlayInfo;
 }
 
-// const defaultExpirationTime = 1;
-const defaultExpirationTime = 60 * 60 * 1000;
-// todo 铲掉这玩意，统一使用 promise-memoize
-const videoMap = new ExpiringMap<string, GetVideoResponse>(defaultExpirationTime);
-
 export interface GetVideoOptions {
     id?: string;
     episode?: string | number;
     url?: string;
 }
 
-export async function getVideo({
-    id,
-    episode = 1,
-    url,
-}: GetVideoOptions): Promise<GetVideoResponse | GetVideoFestivalResponse> {
-    let key = `${id}__${episode}`;
-    const got = videoMap.get(key);
-    if (got && id && episode) {
-        return got;
-    }
-    const raw = await fetch(url || `${BILIBILI_VIDEO_URL_PREFIX}${id}/?p=${episode}`, {
-        headers: {
-            "user-agent": USER_AGENT_BILIBILI,
-        },
-    });
-    const response = await raw.text();
+export const getVideo = promiseMemoize(
+    async ({ id, episode = 1, url }: GetVideoOptions): Promise<GetVideoResponse | GetVideoFestivalResponse> => {
+        const raw = await fetch(url || `${BILIBILI_VIDEO_URL_PREFIX}${id}/?p=${episode}`, {
+            headers: {
+                "user-agent": USER_AGENT_BILIBILI,
+            },
+        });
+        const response = await raw.text();
 
-    log.debug(`最终跳转结果：${raw.url}`);
-    if (raw.url.startsWith("https://www.bilibili.com/festival/")) {
-        log.debug(`按照活动视频处理该查询`);
-        // 提取视频播放信息
-        const initialState: InitialStateFestivalResponse = extractJSON(
-            /window\.__INITIAL_STATE__=(\{.+});\(function\(\)\{/,
-            response,
-        );
-        // 提取视频流信息
-        const playInfo = await getVideoUrlFestival(
-            raw.url,
-            initialState.videoInfo.aid,
-            initialState.videoInfo.bvid,
-            initialState.videoInfo.pages[0].cid,
-        );
+        log.debug(`最终跳转结果：${raw.url}`);
+        if (raw.url.startsWith("https://www.bilibili.com/festival/")) {
+            log.debug(`按照活动视频处理该查询`);
+            // 提取视频播放信息
+            const initialState: InitialStateFestivalResponse = extractJSON(
+                /window\.__INITIAL_STATE__=(\{.+});\(function\(\)\{/,
+                response,
+            );
+            // 提取视频流信息
+            const playInfo = await getVideoUrlFestival(
+                raw.url,
+                initialState.videoInfo.aid,
+                initialState.videoInfo.bvid,
+                initialState.videoInfo.pages[0].cid,
+            );
 
-        // todo 处理没有音频流的情况
-        return { type: "festival", initialState, playInfo };
-    } else {
-        // 提取视频播放信息
-        log.debug(`按照常规视频处理该查询`);
-        const initialState: InitialStateResponse = extractJSON(/window\.__INITIAL_STATE__=(\{.+});/, response);
-        const playInfo: WebPlayInfo = extractJSON(/window\.__playinfo__=(\{.+})<\/script><script>/, response);
-        const getVideoResponse: GetVideoResponse = { type: "regular", initialState, playInfo };
-
-        if (url) {
-            key = `${initialState.bvid}__1`;
+            return { type: "festival", initialState, playInfo };
+        } else {
+            // 提取视频播放信息
+            log.debug(`按照常规视频处理该查询`);
+            const initialState: InitialStateResponse = extractJSON(/window\.__INITIAL_STATE__=(\{.+});/, response);
+            const playInfo: WebPlayInfo = extractJSON(/window\.__playinfo__=(\{.+})<\/script><script>/, response);
+            return { type: "regular", initialState, playInfo };
         }
-        videoMap.set(key, getVideoResponse);
-        return getVideoResponse;
-    }
-}
+    },
+    { maxAge },
+);
