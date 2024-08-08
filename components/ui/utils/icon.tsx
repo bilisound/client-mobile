aba6e69ee8afe1dd34aad47d65ae2e4dec11d459
tcdw <tcdw2011@gmail.com>
2024-08-08 22:44:12 +0800

    消除 any 类型

--- components/ui/utils/icon.tsx
+++ components/ui/utils/icon.tsx
@@ -2,7 +2,7 @@ import { Icon, IconProps } from "@expo/vector-icons/build/createIconSet";
 import React from "react";
 
 export function createIcon<G extends string, FN extends string>(IconComponent: Icon<G, FN>, name: G) {
-    return (iconProps: Partial<IconProps<any>>) => {
+    return (iconProps: Omit<IconProps<G>, "name">): React.ReactElement => {
         return <IconComponent name={name} {...iconProps} />;
     };
 }
