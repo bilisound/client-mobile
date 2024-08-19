72994a679df7ddb20da757859ce0ead4d5d895b1
tcdw <tcdw2011@gmail.com>
2024-08-19 23:21:33 +0800

    让样式工作

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,11 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
-    // [Web-only]: Enables CSS support in Metro.
     isCSSEnabled: true,
 });
-config.resolver.assetExts.push(
-    // Adds support for asset file types
-    "css",
-);
-module.exports = config;
+
+module.exports = withNativeWind(config, {
+    input: "./global.css",
+    inlineRem: 16,
+});
