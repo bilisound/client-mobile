ae32a26e21b8ded2ff3d2bb4ee4925277ec53043
tcdw <tcdw2011@gmail.com>
2024-09-06 23:07:53 +0800

    随机播放功能准备

--- storage/queue.ts
+++ storage/queue.ts
@@ -1,7 +1,15 @@
 import { MMKV } from "react-native-mmkv";
 
+// 当前队列
 export const QUEUE_LIST = "queue_list";
 
+// 当前队列备份（用于退出随机播放模式）
+export const QUEUE_LIST_BACKUP = "queue_list_backup";
+
+// 当前播放的曲目在队列中的 index
 export const QUEUE_CURRENT_INDEX = "queue_current_index";
+
+// 当前队列是否已经随机化
+export const QUEUE_IS_RANDOMIZED = "queue_is_randomized";
 
 export const queueStorage = new MMKV({ id: "storage-queue" });
