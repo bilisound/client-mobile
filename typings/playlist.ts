830b2fdf440cb49f6bf2b983a3de878f4e0dac78
tcdw <tcdw2011@gmail.com>
2024-09-18 22:00:19 +0800

    标记被本地修改过的歌单

--- typings/playlist.ts
+++ typings/playlist.ts
@@ -2,9 +2,11 @@ import { UserListMode } from "~/api/bilisound";
 
 export type PlaylistSource = {
     type: "video";
+    isTainted: boolean;
     bvid: string;
 } & {
     type: "playlist";
+    isTainted: boolean;
     subType: UserListMode;
     userId: string;
     listId: string;
