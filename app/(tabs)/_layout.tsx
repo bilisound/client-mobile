5bd390c2494eee875d93c86a9b5468abd467bde3
tcdw <tcdw2011@gmail.com>
2024-09-30 00:19:35 +0800

    修正新版导航栏动画故障

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,9 @@
 import { Ionicons, FontAwesome5, Entypo } from "@expo/vector-icons";
-import { BlurView as OGBlurView } from "@react-native-community/blur";
+// import { BlurView } from "@react-native-community/blur";
 import { BottomTabBarProps } from "@react-navigation/bottom-tabs/src/types";
 import BottomTabBar from "@react-navigation/bottom-tabs/src/views/BottomTabBar";
 import { CommonActions, NavigationContext, NavigationRouteContext } from "@react-navigation/native";
 import { Tabs } from "expo-router";
-import { cssInterop } from "nativewind";
 import React from "react";
 import { useColorScheme, View } from "react-native";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
@@ -17,10 +16,6 @@ import { Pressable } from "~/components/ui/pressable";
 import { Text } from "~/components/ui/text";
 import useFeaturesStore from "~/store/features";
 import useSettingsStore from "~/store/settings";
-
-const BlurView = cssInterop(OGBlurView, {
-    className: "style",
-});
 
 const IconSearch = createIcon(FontAwesome5, "search");
 const IconSettings = createIcon(Ionicons, "settings-sharp");
@@ -56,7 +51,7 @@ function FloatTabBar(props: BottomTabBarProps) {
     const currentActiveOption = props.descriptors[currentActive.key];
     console.log(JSON.stringify({ currentActive, currentActiveOption }));*/
 
-    const colorScheme = useColorScheme();
+    // const colorScheme = useColorScheme();
 
     return (
         <Box
@@ -68,65 +63,62 @@ function FloatTabBar(props: BottomTabBarProps) {
             }}
         >
             <Box className="m-3 h-16 rounded-full overflow-hidden shadow-lg relative">
-                <BlurView className="w-full h-full" blurAmount={8} blurType={colorScheme || "light"}>
-                    <Box className="w-full h-full flex-row bg-background-0/50">
-                        {props.state.routes.map((route, i) => {
-                            const metadata = props.descriptors[route.key];
-                            const focused = props.state.index === i;
+                {/*<BlurView blurAmount={8} blurType={colorScheme || "light"}>*/}
+                <Box className="w-full h-full flex-row bg-background-0/95">
+                    {props.state.routes.map((route, i) => {
+                        const metadata = props.descriptors[route.key];
+                        const focused = props.state.index === i;
 
-                            const onPress = () => {
-                                const event = props.navigation.emit({
-                                    type: "tabPress",
-                                    target: route.key,
-                                    canPreventDefault: true,
+                        const onPress = () => {
+                            const event = props.navigation.emit({
+                                type: "tabPress",
+                                target: route.key,
+                                canPreventDefault: true,
+                            });
+
+                            if (!focused && !event.defaultPrevented) {
+                                props.navigation.dispatch({
+                                    ...CommonActions.navigate({ name: route.name, merge: true }),
+                                    target: props.state.key,
                                 });
+                            }
+                        };
 
-                                if (!focused && !event.defaultPrevented) {
-                                    props.navigation.dispatch({
-                                        ...CommonActions.navigate({ name: route.name, merge: true }),
-                                        target: props.state.key,
-                                    });
-                                }
-                            };
+                        const onLongPress = () => {
+                            props.navigation.emit({
+                                type: "tabLongPress",
+                                target: route.key,
+                            });
+                        };
 
-                            const onLongPress = () => {
-                                props.navigation.emit({
-                                    type: "tabLongPress",
-                                    target: route.key,
-                                });
-                            };
-
-                            return (
-                                <NavigationContext.Provider
-                                    key={route.key}
-                                    value={props.descriptors[route.key].navigation}
-                                >
-                                    <NavigationRouteContext.Provider value={route}>
-                                        <Pressable
-                                            key={route.key}
-                                            className="flex-1 items-center justify-center gap-1.5"
-                                            onPress={onPress}
-                                            onLongPress={onLongPress}
+                        return (
+                            <NavigationContext.Provider key={route.key} value={props.descriptors[route.key].navigation}>
+                                <NavigationRouteContext.Provider value={route}>
+                                    <Pressable
+                                        key={route.key}
+                                        className="flex-1 items-center justify-center gap-1.5"
+                                        onPress={onPress}
+                                        onLongPress={onLongPress}
+                                    >
+                                        <Box className="w-6 h-6 items-center justify-center">
+                                            {metadata.options.tabBarIcon?.({
+                                                focused,
+                                                color: "",
+                                                size: 0,
+                                            })}
+                                        </Box>
+                                        <Text
+                                            className={`text-xs ${focused ? "text-accent-500 font-semibold" : "text-typography-700"}`}
                                         >
-                                            <Box className="w-6 h-6 items-center justify-center">
-                                                {metadata.options.tabBarIcon?.({
-                                                    focused,
-                                                    color: "",
-                                                    size: 0,
-                                                })}
-                                            </Box>
-                                            <Text
-                                                className={`text-xs ${focused ? "text-accent-500 font-semibold" : "text-typography-700"}`}
-                                            >
-                                                {metadata.options.title}
-                                            </Text>
-                                        </Pressable>
-                                    </NavigationRouteContext.Provider>
-                                </NavigationContext.Provider>
-                            );
-                        })}
-                    </Box>
-                </BlurView>
+                                            {metadata.options.title}
+                                        </Text>
+                                    </Pressable>
+                                </NavigationRouteContext.Provider>
+                            </NavigationContext.Provider>
+                        );
+                    })}
+                </Box>
+                {/*</BlurView>*/}
             </Box>
         </Box>
     );
