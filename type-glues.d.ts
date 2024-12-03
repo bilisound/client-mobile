e510fd40352516bbb1aa2c1efdfd6a443ea2a603
tcdw <tcdw2011@gmail.com>
2024-12-03 21:20:17 +0800

    特性暂时屏蔽

--- type-glues.d.ts
+++ type-glues.d.ts
@@ -11,5 +11,6 @@ declare const process: {
         NODE_ENV: string;
         EXPO_PUBLIC_API_URL: string;
         EXPO_PUBLIC_RELEASE_CHANNEL: ReleaseChannel;
+        EXPO_FEATURES_ANDROID_RIPPLE_BUTTON: "true" | "false";
     };
 };
