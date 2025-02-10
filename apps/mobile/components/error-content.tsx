e80c22c51442997fe28ebf7ff923459d8cfd96a4
tcdw <tcdw2011@gmail.com>
2025-02-10 21:15:36 +0800

    修正错误图标颜色

--- apps/mobile/components/error-content.tsx
+++ apps/mobile/components/error-content.tsx
@@ -1,15 +1,18 @@
 import { View } from "react-native";
 import { Monicon } from "@monicon/native";
 import { Text } from "~/components/ui/text";
+import { useRawThemeValues } from "~/components/ui/gluestack-ui-provider/theme";
 
 export interface ErrorContentProps {
     message: string;
 }
 
 export function ErrorContent({ message }: ErrorContentProps) {
+    const { colorValue } = useRawThemeValues();
+
     return (
         <View className={"gap-4 items-center p-4"}>
-            <Monicon name={"tabler:alert-square-rounded"} className={"color-typography-400"} size={64} />
+            <Monicon name={"tabler:alert-square-rounded"} color={colorValue("--color-typography-400")} size={64} />
             <Text className={"color-typography-400 text-base leading-normal"}>{message}</Text>
         </View>
     );
