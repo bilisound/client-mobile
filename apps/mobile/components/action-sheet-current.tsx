1470b19f16236eac4921f8931e4a8acb9f977e50
tcdw <tcdw2011@gmail.com>
2025-02-16 23:37:28 +0800

    显式声明 w-full

--- apps/mobile/components/action-sheet-current.tsx
+++ apps/mobile/components/action-sheet-current.tsx
@@ -12,7 +12,7 @@ export interface ActionSheetCurrentProps {
 
 export function ActionSheetCurrent({ line1, line2, image }: ActionSheetCurrentProps) {
     return (
-        <View className={"flex-row items-center px-4 py-4 gap-4"}>
+        <View className={"flex-row items-center px-4 py-4 gap-4 w-full"}>
             {image ? <Image source={image} className={"h-12 aspect-[3/2] rounded-lg flex-0 basis-auto"} /> : null}
             <View className="flex-1 items-start w-full gap-1.5">
                 <Text className="font-bold" isTruncated>
