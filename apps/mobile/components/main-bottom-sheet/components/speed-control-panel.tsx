400f171cdcf4cb653f9e52bc5b6bbde302572ba0
tcdw <tcdw2011@gmail.com>
2025-03-19 21:39:42 +0800

    改进速度调节界面

--- apps/mobile/components/main-bottom-sheet/components/speed-control-panel.tsx
+++ apps/mobile/components/main-bottom-sheet/components/speed-control-panel.tsx
@@ -13,8 +13,8 @@ export function SpeedControlPanel() {
 
     return (
         <>
-            <Text className={"font-semibold text-base text-center"}>{speedValue.toFixed(2) + "x"}</Text>
-            <View className={"h-6 flex-row items-center gap-4 mt-4"}>
+            <Text className={"font-semibold text-xl text-center py-1"}>{speedValue.toFixed(2) + "x"}</Text>
+            <View className={"h-6 flex-row items-center gap-4 mt-4 px-2"}>
                 <GSSlider
                     className={"flex-1"}
                     value={speedValue}
