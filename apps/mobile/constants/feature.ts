2aeb94089c8f0537ff3919b02775168685cfbe66
tcdw <tcdw2011@gmail.com>
2025-04-04 21:41:40 +0800

    2.2.1

--- apps/mobile/constants/feature.ts
+++ apps/mobile/constants/feature.ts
@@ -5,9 +5,9 @@ const NOT_PROD_READY = RELEASE_CHANNEL === "android_github_beta" || process.env.
 /**
  * 批量下载歌单或多分 P 视频
  */
-export const FEATURE_MASS_DOWNLOAD = NOT_PROD_READY;
+export const FEATURE_MASS_DOWNLOAD = true;
 
 /**
  * 下载管理器
  */
-export const FEATURE_DOWNLOAD_MANAGER = NOT_PROD_READY;
+export const FEATURE_DOWNLOAD_MANAGER = true;
