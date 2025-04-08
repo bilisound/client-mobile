83485cf7a5db8a777e21d7e191f512f293b9bd73
tcdw <tcdw2011@gmail.com>
2025-04-08 15:12:11 +0800

    使用新的检查更新接口检查更新

--- apps/mobile/business/check-release.ts
+++ apps/mobile/business/check-release.ts
@@ -1,9 +1,7 @@
-import * as semver from "semver";
-
-import { USER_AGENT_BILISOUND } from "~/constants/network";
 import { RELEASE_CHANNEL } from "~/constants/releasing";
 import ReactNativeBlobUtil from "react-native-blob-util";
 import { BRAND } from "~/constants/branding";
+import { getUpdate } from "~/api/bilisound";
 
 export interface CheckLatestVersionReturns {
     isLatest: boolean;
@@ -14,42 +12,19 @@ export interface CheckLatestVersionReturns {
     downloadUrl: string;
 }
 
-export async function checkLatestVersionGithub(
-    currentVersion: string,
-    includePrerelease = false,
-): Promise<CheckLatestVersionReturns> {
-    const endpoint = includePrerelease
-        ? "https://api.github.com/repos/bilisound/client-mobile/releases"
-        : "https://api.github.com/repos/bilisound/client-mobile/releases/latest";
-    const response = await fetch(endpoint, {
-        headers: {
-            "User-Agent": USER_AGENT_BILISOUND,
-        },
-    }).then(e => e.json());
-    const latestRelease = includePrerelease ? response.find((r: any) => r.prerelease) || response[0] : response;
-    const latestVersion = latestRelease.tag_name.replace("v", "");
-
-    if (!semver.valid(currentVersion) || !semver.valid(latestVersion)) {
-        throw new Error("Invalid version format");
-    }
-
-    return {
-        isLatest: semver.gte(currentVersion, latestVersion),
-        currentVersion,
-        latestVersion,
-        extraInfo: "",
-        downloadPage: latestRelease.html_url,
-        downloadUrl: latestRelease.assets[0].browser_download_url,
-    };
-}
-
 export async function checkLatestVersion(currentVersion: string) {
     switch (RELEASE_CHANNEL) {
-        case "android_github": {
-            return checkLatestVersionGithub(currentVersion, false);
-        }
+        case "android_github":
         case "android_github_beta": {
-            return checkLatestVersionGithub(currentVersion, true);
+            const { data: result } = await getUpdate();
+            return {
+                isLatest: currentVersion === result.version,
+                currentVersion,
+                latestVersion: result.version,
+                extraInfo: result.info,
+                downloadPage: result.downloadPage,
+                downloadUrl: result.downloadUrl,
+            };
         }
         default: {
             return {
