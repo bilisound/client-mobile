9de39b66f4d28d5bf371a888ce3bf8f8e303794a
tcdw <tcdw2011@gmail.com>
2025-04-09 16:28:34 +0800

    加白名单

--- apps/server-cf/middleware/withRefererCheck.ts
+++ apps/server-cf/middleware/withRefererCheck.ts
@@ -3,6 +3,8 @@ import { ajaxError } from "../utils/misc";
 
 const whitelist = [
     /^https:\/\/(?:[\w-]+\.)*bilisound\.moe(?:\/[\w/.-]*)?$/,
+    /^https:\/\/(?:[\w-]+\.)*bilisound\.com(?:\/[\w/.-]*)?$/,
+    /^https:\/\/(?:[\w-]+\.)*client-mobile.pages.dev(?:\/[\w/.-]*)?$/,
     /^http:\/\/(?:[\w-]+\.)*localhost:\d{4,5}(?:\/[\w/.-]*)?$/,
 ];
 
