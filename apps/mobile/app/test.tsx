2b0d89b23fb3469690c2895ffbc1da7aa167efce
tcdw <tcdw2011@gmail.com>
2025-10-02 18:33:13 +0800

    feat: testing android ripple errors

--- apps/mobile/app/test.tsx
+++ apps/mobile/app/test.tsx
@@ -3,7 +3,16 @@ import { Box } from "~/components/ui/box";
 import { Text } from "~/components/ui/text";
 import { useState } from "react";
 import { SystemBars, SystemBarStyle } from "react-native-edge-to-edge";
-import { Button, Platform, ScrollView, useColorScheme as useColorSchemeRN, View } from "react-native";
+import {
+  Button,
+  Platform,
+  Pressable,
+  ScrollView,
+  TouchableNativeFeedback,
+  useColorScheme as useColorSchemeRN,
+  View,
+  StyleSheet,
+} from "react-native";
 import { shadow } from "~/constants/styles";
 import { Layout } from "~/components/layout";
 import init from "~/utils/init";
@@ -62,6 +71,53 @@ export default function Test() {
               <Button title={"NW Shadow"} onPress={() => setNativeWindShadow(1)} />
               <Button title={"Off"} onPress={() => setNativeWindShadow(2)} />
             </Box>
+            <Box className={"flex gap-4 py-4"}>
+              <Pressable
+                android_ripple={{
+                  color: "#ffffff",
+                }}
+                className="bg-blue-200 w-48 h-16 rounded-lg justify-center items-center"
+              >
+                <Text>Pressable</Text>
+              </Pressable>
+              <TouchableNativeFeedback
+                style={{
+                  borderRadius: 8,
+                  padding: 16,
+                  backgroundColor: "green",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                background={TouchableNativeFeedback.Ripple("#ffffff", false)}
+              >
+                <Text>TouchableNativeFeedback</Text>
+              </TouchableNativeFeedback>
+            </Box>
+            <View style={styles.container}>
+              <View style={styles.textContainer}>
+                <Text>Broken behavior without foreground:</Text>
+              </View>
+              <Pressable
+                android_ripple={{ color: "blue", foreground: false }}
+                onPress={() => console.log("Pressed ripple bg!")}
+              >
+                <View style={styles.pressableContent}>
+                  <Text>Broken ripple without foreground</Text>
+                </View>
+              </Pressable>
+
+              <View style={styles.textContainer}>
+                <Text>Normal behavior when using foreground true</Text>
+              </View>
+              <Pressable
+                android_ripple={{ color: "blue", foreground: true }}
+                onPress={() => console.log("Pressed ripple fg!")}
+              >
+                <View style={styles.pressableContent}>
+                  <Text>Working on foreground</Text>
+                </View>
+              </Pressable>
+            </View>
             {nativeWindShadow === 0 && (
               <Box className={"gap-12 mt-8 pb-8"} key={"test-0"}>
                 <Box className={"bg-blue-500 w-48 h-16 rounded-lg"} style={{ boxShadow: shadow.sm }} />
@@ -106,3 +162,25 @@ export default function Test() {
     </Layout>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FCF9F5",
+    margin: 16,
+  },
+  textContainer: {
+    marginTop: 24,
+    alignItems: "center",
+  },
+  pressableContent: {
+    width: "100%",
+    height: 250,
+    padding: 16,
+    marginTop: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
